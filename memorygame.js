78b9bd847a5949b8aee8a5e81c68230925b882fb
AmosCohen <amoshirac@gmail.com>
2022-04-05 19:13:40 +0300

    add shuffle finction

--- memorygame.js
+++ memorygame.js
@@ -1,10 +1,30 @@
 const cards=["amos","shira","Elyashiv","harel","beery","nave","ori","amos","shira","Elyashiv","harel","beery","nave","ori"]
 //TODO
-
-const board = document.getElementById("board");
-
-for (i in cards){
-    const element = document.createElement("div");
-    element.innerHTML=cards[i];
-    board.appendChild(element);
+function createCard(idx){
+    const cardEl = document.createElement("div");
+    cardEl.innerHTML=cards[idx];
+    cardEl.id=idx;
+    return cardEl
 }
+
+function shuffle(arr) {
+        for (let i = arr.length - 1; i > 0; i--) {
+            // Generate random number
+            let j = Math.floor(Math.random() * (i + 1));
+                        
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }    
+        return arr;
+        }
+
+//function init(){
+    shuffle(cards)
+    const board = document.getElementById("board");
+    for (i in cards){
+        const element=createElement[i];
+        board.appendChild(element);
+    }
+//}
+//init()
