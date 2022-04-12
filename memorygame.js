575b25e8aa09a9d34201f686b8b4a2cab88be23a
AmosCohen <amoshirac@gmail.com>
2022-04-12 20:02:56 +0300

    add card match

--- memorygame.js
+++ memorygame.js
@@ -1,18 +1,26 @@
 const cards=["amos","shira","Elyashiv","harel","beery","nave","ori","amos","shira","Elyashiv","harel","beery","nave","ori"]
 const board = document.getElementById("board");
-//TODO
-function createCard(idx){
+let counter=0;
+let card1='';
+let card2='';
+let success=0;
+
+
+function createCard(idx){//פונקציה ליצירת כרטיס
     const cardEl = document.createElement("div");
-//  cardEl.innerHTML=cards[idx];
     cardEl.id=idx;
     cardEl.className="cardsClass"
-    cardEl.onclick = e => {
+    cardEl.addEventListener('click',()=> {//זה כשהקלף מתהפך
         cardEl.innerText = cards[idx]
-    }
+        counter++
+        if (counter==1?card1= idx:card2= idx) {
+            // isMatch(counter)
+        }
+    })
     return cardEl
 }
 
-function shuffle(arr) {
+function shuffle(arr) {//פונקציה שמערבבת את הכרטיסים
         for (let i = arr.length - 1; i > 0; i--) {
             // Generate random number
             let j = Math.floor(Math.random() * (i + 1));         
@@ -23,18 +31,32 @@ function shuffle(arr) {
         return arr;
         }
 
-//function init(){
-    shuffle(cards);
-    for (i in cards){
-        const element=createCard(i);
-        // element.innerText = cards[i]
+// function isMatch(counter) {
+//     if (counter==2) {
+//         if (card1==card2) {
+//             success++
+//             //לטפל בקלפים
 
-        board.appendChild(element);
-    }
-//}
-//init()
-// function cardOnClick() {
-//     cardEl.addEventListener('click', (cardEl) => {
-//         cardEl.innerText = cards[i]
-//         })
+//         }
+//         else{
+//             //להפוך בחזרה
+//         }
+//        card1='' 
+//        card2='' 
+//        counter=0
+//     }
 // }
+
+shuffle(cards);
+for (i in cards){//יצירת הכרטיסים בפועל- מכניס לאלמנט ואז מכניס את האלמנט ללוח
+    const element=createCard(i);
+    board.appendChild(element);
+}
+
+// while (success<7) {
+//     isMatch(counter)
+// }
+// alert("well donde")
+
+
+
