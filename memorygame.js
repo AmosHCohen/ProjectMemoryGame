06acc82dfe22d15e1667b3d8a01a427727f110df
AmosCohen <amoshirac@gmail.com>
2022-04-12 18:41:02 +0300

    add on click function

--- memorygame.js
+++ memorygame.js
@@ -3,18 +3,19 @@ const board = document.getElementById("board");
 //TODO
 function createCard(idx){
     const cardEl = document.createElement("div");
-    cardEl.innerHTML=cards[idx];
+//  cardEl.innerHTML=cards[idx];
     cardEl.id=idx;
     cardEl.className="cardsClass"
-
+    cardEl.onclick = e => {
+        cardEl.innerText = cards[idx]
+    }
     return cardEl
 }
 
 function shuffle(arr) {
         for (let i = arr.length - 1; i > 0; i--) {
             // Generate random number
-            let j = Math.floor(Math.random() * (i + 1));
-                        
+            let j = Math.floor(Math.random() * (i + 1));         
             let temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
@@ -26,7 +27,14 @@ function shuffle(arr) {
     shuffle(cards);
     for (i in cards){
         const element=createCard(i);
+        // element.innerText = cards[i]
+
         board.appendChild(element);
     }
 //}
 //init()
+// function cardOnClick() {
+//     cardEl.addEventListener('click', (cardEl) => {
+//         cardEl.innerText = cards[i]
+//         })
+// }
